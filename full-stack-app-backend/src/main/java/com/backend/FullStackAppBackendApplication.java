2110e0f3830e6a139d79b28e29da9625b7710350
rajat <baeren.rajat@alfaitoutsourcing.de>
2024-11-09 22:02:42 +0530

    backend added

---
+++ full-stack-app-backend/src/main/java/com/backend/FullStackAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppBackendApplication.class, args);
+	}
+
+}
